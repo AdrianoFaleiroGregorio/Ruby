f8df561b8f36f414a714ae1c85ea6173e38bc192
AdrianoFG <adrianofgtrabalho@gmail.com>
2023-10-13 00:47:20 -0300

    Atualização Ex3

--- One Bit Code/aula4/ex3.rb
+++ One Bit Code/aula4/ex3.rb
@@ -1,3 +1,25 @@
+# Resposta com select
+
+numbers = {'A' => 10, 'B' => 302, 'C' => 20, 'D' => 25, 'E' => 15}
+
+value1 = 0
+result = []
+
+numbers.select do |key, value|
+    if value > value1
+        value1 = value
+        result = [key, value]
+    end
+end
+
+p "Chave: #{result[0]} Valor: #{result[1]}"
+
+
+
+
+
+# Resposta com each
+
 numbers = {'A' => 10, 'B' => 30, 'C' => 20, 'D' => 25, 'E' => 15}
 
 value1 = 0
