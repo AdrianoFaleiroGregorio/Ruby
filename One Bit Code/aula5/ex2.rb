9820d8f35e9bfa35bb0b3f269f6ab22b98630709
AdrianoFG <adrianofgtrabalho@gmail.com>
2023-10-21 17:42:49 -0300

    Inicio ex2

---
+++ One Bit Code/aula5/ex2.rb
@@ -0,0 +1,9 @@
+#Siga a documentação da gem CPF_CNPJ para criar um programa que
+#receba como entrada um número de CPF e em um método verifique se
+#este número é válido.
+
+def veficação (CPF)
+
+    CPF_CNPJ
+
+end
