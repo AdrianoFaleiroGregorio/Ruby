44b9bf323019fad93a8cf9eb5ec9a143ba155f13
AdrianoFG <adrianofgtrabalho@gmail.com>
2023-10-22 18:22:03 -0300

    Ex2  da aula5 corrigido e funcional.

--- One Bit Code/aula5/ex2.rb
+++ One Bit Code/aula5/ex2.rb
@@ -2,8 +2,21 @@
 #receba como entrada um número de CPF e em um método verifique se
 #este número é válido.
 
-def veficação (CPF)
+require 'cpf_cnpj'
 
-    CPF_CNPJ
+def verificacao (cpf)
+    return CPF.valid?(cpf)
+end
 
+p 'Digite um CPF ou CNPJ válido.'
+cpf_cliente = gets.chomp
+
+cpf_valido = verificacao(cpf_cliente)
+
+if cpf_valido
+    p "CPF #{cpf_cliente} valido!"
+    cpf_formatado = CPF.new(cpf_cliente).formatted
+    p "CPF formatado: #{cpf_formatado}"
+else
+    p 'CPF não é válido!'
 end
